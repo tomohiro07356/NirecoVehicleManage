586131bda9b4e772e497ea8d54a9a6cafef51840
tomohiro07356 <69028818+tomohiro07356@users.noreply.github.com>
2023-07-24 17:09:15 +0900

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from controllers import *
 
 # FastAPIのルーティング用関数
-app.add_api_route('/', index)
+app.add_api_route('/', index_)
 app.add_api_route('/admin', admin)
 app.add_api_route('/admin/dateinfo', get_dateinfo, methods=['POST'])
 app.add_api_route('/admin/dateinfo_error', get_dateinfo_error, methods=['POST'])
@@ -9,6 +9,3 @@ app.add_api_route('/admin/monthly_record', get_monthly_record, methods=['POST'])
 app.add_api_route('/download_employee', history_download_employee)
 app.add_api_route('/download_nonemployee', history_download_nonemployee)
 app.add_api_route('/add_holiday', add_holiday, methods=['POST'])
-
-app.add_api_route('/debug', fetch_background_task, methods=['GET'])
-app.add_api_route('/debug', execute_background_task, methods=['POST'])
