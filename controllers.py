4942efa5422ab66bb70fb0640133969f29e2340d
tomohiro <tomohiro07356@gmail.com>
2025-02-20 08:56:41 +0900

    fix

--- controllers.py
+++ controllers.py
@@ -693,11 +693,10 @@ def history_download_nonemployee(request: Request):
     
     # 利用日を追加
     lis_histdate_fix, nID_fix, nName_fix, n_number_fix = [],[],[],[]
-  
-    lis_histdate_sort = []
-    for i in range(len(lis_of_lis_histdate)):
-        if len(lis_of_lis_histdate[i]) > 1:
-            lis_of_lis_histdate[i].sort()
+
+    for histdate in lis_of_lis_histdate:
+        if len(histdate) > 1:
+            histdate.sort()
 
     for i in range(len(lis_of_lis_histdate)):
         date_cnt = len(lis_of_lis_histdate[i])
